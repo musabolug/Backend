5c2b589560f9c751fb078b46729cb095624bc69a
musabolug <musabolug@gmail.com>
2023-04-06 14:10:58 +0300

    Static files uploaded

--- NodeJs/CleanBlog/app.js
+++ NodeJs/CleanBlog/app.js
@@ -1,12 +1,15 @@
-const express = require("express")
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const app = express()
+app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.send("Merhaba")
-})
+app.get("/", (req, res) => {
+//   res.send("CleanBlog a hosgeldin");
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
 
-const port = 3002
-app.listen(port,()=>{
-    console.log(`CleanBlog sever port ${port} da baslatildi`)
-})
+const port = 3001;
+app.listen(port, () => {
+  console.log(`CleanBlog sever port ${port} da baslatildi`);
+});
