ac2d5d6a7fc5f277dc4fb03a4549c9e3e3235190
musabolug <musabolug@gmail.com>
2023-04-06 15:38:41 +0300

    template engine

--- NodeJs/CleanBlog/app.js
+++ NodeJs/CleanBlog/app.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
+
 const app = express();
 
+//*TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//*MIDDLEWARES
 app.use(express.static("public"));
 
+//*ROUTES
 app.get("/", (req, res) => {
-//   res.send("CleanBlog a hosgeldin");
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
+});
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+app.get("*", (req, res) => {
+    res.send(404,"404 not found")
 });
 
 const port = 3001;
