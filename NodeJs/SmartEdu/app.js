5b88369d7610bf6ab1d77f9f8db21a2578a6f745
musabolug <musabolug@gmail.com>
2023-04-11 10:07:33 +0300

    template engine index and about files are fixed

--- NodeJs/SmartEdu/app.js
+++ NodeJs/SmartEdu/app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Index Page")
+//* Template Engine
+app.set("view engine", "ejs");
+
+//*MIDDLEWARES
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+  res.status(200).render("index",{
+    page_name : "index"
+  })
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about",{
+    page_name : "about"
 })
+});
 
 const port = 3001;
 app.listen(port, () => {
